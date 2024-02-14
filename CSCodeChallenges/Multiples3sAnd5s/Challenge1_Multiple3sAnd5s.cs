757b073bf8dcda40b4b915f2a82c2809e46f6cba
HossinatorWCheez <ethanjames.aa@outlook.com>
2024-02-14 15:36:15 -0600

    Completed Challenge1

---
+++ CSCodeChallenges/Multiples3sAnd5s/Challenge1_Multiple3sAnd5s.cs
@@ -0,0 +1,21 @@
+﻿namespace Multiples3sAnd5s
+{
+    internal class Challenge1_Multiple3sAnd5s
+    {
+        static void Main(string[] args)
+        {
+            int below10 = 3 + 5 + 6 + 9;
+            Console.WriteLine(below10);
+
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
